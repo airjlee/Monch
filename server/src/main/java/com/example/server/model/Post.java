436dfeb7f79016ae6545c5886091c08315c6f74f
airjlee <airjlee04@gmail.com>
2024-07-10 00:12:25 -0700

    created post jparepo and model

--- server/src/main/java/com/example/server/model/Post.java
+++ server/src/main/java/com/example/server/model/Post.java
@@ -4,38 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 public class Post {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String title;
+    private String userName;
     private String content;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
+    private String rating;
+    private String restaurantName;
+    private String imageUrl;
+    private String caption;
 }
